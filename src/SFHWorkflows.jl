997f6222d9ef09ad72e023f7d277404229fffbc1
cgarling <chris.t.garling@gmail.com>
2025-11-18 12:37:25 -0500

    revise makie linewidth

--- src/SFHWorkflows.jl
+++ src/SFHWorkflows.jl
@@ -4,18 +4,21 @@ export fit_sfh
 
 using StatsBase: Histogram
 using CairoMakie: Makie, set_theme!, theme_latexfonts # CairoMakie re-exports Makie
-set_theme!(theme_latexfonts(); 
-        fontsize=20,
-        Axis = (xticks = Makie.LinearTicks(5),
-                yticks = Makie.LinearTicks(7),
-                # xticks=Makie.WilkinsonTicks(10; k_min=5, k_max=5),
-                # yticks=Makie.WilkinsonTicks(5; k_min=5, k_max=5)))
-                # xminorticks=Makie.IntervalsBetween(5),
-                xminorticksvisible=true),
-        Scatter = (strokecolor=:black, strokewidth=1),
-        Lines   = (linewidth=5,))
-                # xminorgridvisible=true, 
-                # yminorgridvisible=true))
+
+function __init__()
+    set_theme!(theme_latexfonts(); 
+            fontsize=20,
+            Axis = (xticks = Makie.LinearTicks(5),
+                    yticks = Makie.LinearTicks(7),
+                    # xticks=Makie.WilkinsonTicks(10; k_min=5, k_max=5),
+                    # yticks=Makie.WilkinsonTicks(5; k_min=5, k_max=5)))
+                    # xminorticks=Makie.IntervalsBetween(5),
+                    xminorticksvisible=true),
+            Scatter = (strokecolor=:black, strokewidth=1),
+            Lines   = (linewidth=3,))
+                    # xminorgridvisible=true, 
+                    # yminorgridvisible=true))
+end
 
 function Makie.convert_arguments(P::Makie.CellGrid, h::Histogram)
     return Makie.convert_arguments(P, h.edges[1], h.edges[2], h.weights)
